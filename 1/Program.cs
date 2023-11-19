27bfe539d1a2154ca8a8783c1b6bb9bec6509f84
Artem <koptelovu@gmail.com>
2023-11-19 16:04:42 +0300

    Решены задачи 1, 2, 3

---
+++ 1/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+void printNumbersFromNTo1(int n)
+{
+    if (n == 1)
+    {
+        Console.Write($"{n}\"");
+        return;
+    }
+    Console.Write($"{n}, ");
+    printNumbersFromNTo1(n - 1);
+}
+
+int n;
+n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"N = {n} -> \"");
+
+printNumbersFromNTo1(n);
---
+++ 2/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int sumFromMToN(int m, int n)
+{
+    if (m == n)
+        return n;
+    return m + sumFromMToN(m + 1, n);
+}
+
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+int result = sumFromMToN(m, n);
+
+Console.WriteLine($"M = {m}; N = {n} -> {result}");
